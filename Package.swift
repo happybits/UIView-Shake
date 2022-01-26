1564091080a333d426ac524ff2d4532281360bd0
Joe DeCapo <joe@polka.cat>
2022-01-26 12:54:26 -0600

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "UIViewShake",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "UIViewShake", targets: ["UIViewShake"])
+    ],
+    targets: [
+        .target(
+            name: "UIViewShake",
+            path: "Source",
+            publicHeadersPath: "./"
+        )
+    ]
+)
